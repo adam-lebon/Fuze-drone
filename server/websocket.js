7720dd3242b252a350f43312f916629bf0298890
Adam Le Bon <lebon.adam@gmail.com>
2017-01-05 08:28:48 +0000

    Creation d'une interface web de test pwm

--- server/websocket.js
+++ server/websocket.js
@@ -1,26 +1,19 @@
 var app = require('./webserver.js');
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-
+var pwm = require('./pwm.js');
 
 http.listen(3000);
 
 
-var channels = [
-  1500,
-  1500,
-  1500,
-  1500
-];
-
 io.on('connection', function(socket){
   console.log("New user");
-  socket.emit('setValue', channels);
+  socket.emit('setValue', pwm.channels);
 
   socket.on('newValue', function(data){
-    console.log('new value');
-    channels = data;
-    io.emit('setValue', channels);
+    pwm.channels = data;
+    io.emit('setValue', pwm.channels);
+    pwm.update(pwm.channels);
   });
 
 });
