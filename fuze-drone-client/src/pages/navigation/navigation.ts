1e2cb283ab05179c8367a4da953b1daec4a345e1
Unknown <matteo.lecuit@laposte.net>
2017-02-07 16:28:16 +0100

    Création du composant vide

--- fuze-drone-client/src/pages/navigation/navigation.ts
+++ fuze-drone-client/src/pages/navigation/navigation.ts
@@ -1,13 +1,43 @@
-import { Component } from '@angular/core';
-import { HomePage} from '../home/home';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Platform } from 'ionic-angular';
+import { ScreenOrientation, StatusBar } from 'ionic-native';
+import * as nipplejs from 'nipplejs';
 
 @Component({
   selector: 'navigation',
   templateUrl: 'navigation.html'
 })
 
-export class NavigationPage {
-constructor() { }
+export class NavigationPage implements OnInit, OnDestroy {
+  manager:any;
 
+  constructor(private platform:Platform) { }
+
+  ngOnInit(){
+    this.manager = nipplejs.create({
+      zone: document.getElementById('joystickLeft'),
+      color: "#FF0000",
+      mode: "semi",
+      catchDistance: 50
+    });
+    this.manager = nipplejs.create({
+      zone: document.getElementById('joystickRight'),
+      color: "#00FF00",
+      mode: "semi",
+      catchDistance: 50
+    });
+
+    if(this.platform.is('mobile')) {
+      ScreenOrientation.lockOrientation('landscape');
+      StatusBar.hide();
+    }
+  }
+
+  ngOnDestroy(){
+    if(this.platform.is('mobile')) {
+    ScreenOrientation.unlockOrientation();
+      StatusBar.show();
+    }
+  }
 
   }
